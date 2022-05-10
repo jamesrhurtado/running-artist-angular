d7474a51917046694754a518f4fd55909b40ae16
jrenzzo <renzo-hg03@hotmail.com>
2022-05-10 11:12:49 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'upc-pre-202201-si652-examen-parcial';
+}
